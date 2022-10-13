ba244f51587770da934361595673d3a8bda69b4e
armindnwyr <himkaneki@gmail.com>
2022-10-13 00:38:09 -0500

    Creacion de permisos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -18,6 +18,7 @@ class RoleSeeder extends Seeder
     {
        $role1 = Role::create(['name' => 'Admin']);
        $role2 = Role::create(['name' => 'Boss']);
+       $role2 = Role::create(['name' => 'Asistente']);
 
        Permission::create(['name' => 'home']);
        /*PERMISON PARA DOCENTE*/
