40a6024e0ac9ee82555eb3bfc57dfa4ab546125c
armindnwyr <himkaneki@gmail.com>
2022-10-18 01:52:10 -0500

    Blade libro

--- resources/views/libro/index.blade.php
+++ resources/views/libro/index.blade.php
@@ -9,21 +9,29 @@
 @section('content')
 <a href="{{route('libro.create')}}" class="btn btn-info mb-3">Crear Nuevo Libro</a>
 
-
+<div class="container">
+<div class="row">
 @foreach ($libros->chunk(3) as $chunk)
 <div class="card-group">
   @foreach ($chunk as $li)
   <div class="card m-2">
+  <div class="col">
     <div class="card-body">
       <h5 class="card-title">{{$li->li_titulo}}</h5>
       <p class="card-text">{{$li->li_descripcion}}</p>
-      
+      <div class="df aling-items-start">
+          <a href="{{$li->li_enlace}}" class="btn btn-outline-dark btn-sm" target="_blank"><i class="fas fa-lg fa-file"></i></a></td>
+          <a href="{{route('libro.edit', $li)}}" class="btn btn-outline-success btn-sm"><i class="fas fa-lg fa-edit"></i></a></td>
+          <form action="{{route('libro.destroy', $li)}}" method="post"> @csrf @method('delete') <button type="submit" class="btn btn-outline-danger btn-sm"><i class="fas fa-lg fa-trash"></i></button></form>
+      </div>    
     </div>
   </div>
-  @endforeach
 </div>
 @endforeach
-
+</div>
+@endforeach
+</div>
+</div>
 
 @stop
 
