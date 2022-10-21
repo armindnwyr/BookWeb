8b5a55096431e469045f9d9ebf46e9f6a3090423
armindnwyr <himkaneki@gmail.com>
2022-10-21 11:48:45 -0500

    npm install css y js no creado: razon no se

--- resources/views/libro/index.blade.php
+++ resources/views/libro/index.blade.php
@@ -6,7 +6,6 @@
     <h1>Lista de libros</h1>
 @stop
 
-
 @section('content')
 <a href="{{route('libro.create')}}" class="btn btn-info mb-3">Crear Nuevo Libro</a>
 <div class="card">
@@ -66,15 +65,13 @@
 </div>
 </div> --}}
 
+
 @section('css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
+
 @stop
 
+
 @section('js')
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
 <script>
     $(document).ready(function () {
         $('#libro').DataTable();
