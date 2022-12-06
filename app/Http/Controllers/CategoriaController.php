993b8ae48ca683c1891aa11cadb796442baaaec3
yerson joel fonseca lizarraga <yersonjoelfl@gmail.com>
2022-12-05 22:29:00 -0500

    permisos de logueo

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,6 +12,11 @@ class CategoriaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $categorias = Categoria::all();
--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -15,6 +15,10 @@ class UsuarioController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $usuarios = User::all();
