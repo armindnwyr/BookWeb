9a8fa61060c5ed1c57ead4384a105aa7a0ab1dc6
Cristian <himkaneki@gmail.com>
2023-01-07 20:33:31 -0500

    Roles y Permisos terminado v2

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Usuario')
 
 @section('content_header')
-    <h1 class="text-center">Editar Roles</h1>
+    <h1 class="text-center font-weight-bold text-uppercase">Editar Roles</h1>
 @stop
 
 @section('content')
