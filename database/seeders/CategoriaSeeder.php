e952e0c28cfe4d6c289a40765bcc366a983653fc
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-06-15 16:45:47 -0500

    seeder categoria

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\categoria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        categoria::factory(2)->create();
+        
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,9 +25,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        categoria::factory(2)->create();
 
         $this->call(RoleSeeder::class);
+        $this->call(CategoriaSeeder::class);
         $this->call(DocenteSeeder::class);
         $this->call(AutorSeeder::class);
         $this->call(LibroSeeder::class);
